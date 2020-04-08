890801d5ac6d51dad45eeb4f566730deb093e234
Mark Robertson <markrobertson@u.boisestate.edu>
2020-04-08 08:50:58 -0600

    Add flag to mask zeros from calculations, for data in which zero has meaning

--- tablizer/tablizer.py
+++ tablizer/tablizer.py
@@ -1,54 +1,53 @@
 # -*- coding: utf-8 -*-
-
 import os
-from sys import exit
 import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+
 from tablizer.inputs import Inputs, Base
 from tablizer.defaults import Units, Methods, Fields
 from tablizer.tools import create_sqlite_database, check_inputs_table, insert, \
-        make_session, check_existing_records, delete_records, make_cnx_string
+    make_session, check_existing_records, delete_records, make_cnx_string
 
-def summarize(input, date, methods, percentiles = [25,75], decimals = 3,
-              masks = None):
-    '''
+
+def summarize(array, date, methods, percentiles=[25, 75], decimals=3,
+              masks=None, mask_zero_values=False):
+    """
     Calculate basic summary statistics for 2D arrays or DataFrames.
 
     Args
     ------
-    input : 2D array or DataFrame
-    date : str ('2019-8-18 23:00'), anything pd.to_datetime() can parse
-    methods : list (['mean','std']), strings of numpy functions to apply
-    percentiles : list ([low, high]), must supply when using 'percentile'
-    decimals : int
-    masks :
+    array {arr}: 2D array or DataFrame
+    date {str}: ('2019-8-18 23:00'), anything pd.to_datetime() can parse
+    methods {list}: (['mean','std']), strings of numpy functions to apply
+    percentiles {list}: ([low, high]), must supply when using 'percentile'
+    decimals {int}: rounding
+    masks {list}: mask outputs
+    mask_zero_values {bool}: mask zero values in array
 
     Returns
     ------
-    result : pd.DataFrame, index = date, columns = methods
-
-    '''
+    result {DataFrame}: index = date, columns = methods
+    """
 
     method_options = Methods.options
 
-    if type(methods) != list:
-        methods = [methods]
+    if not isinstance(methods, list):
+        raise TypeError("methods must be a list")
 
-    if type(input) not in [np.ndarray, pd.core.frame.DataFrame]:
-        raise Exception('input type {} not valid'.format(type(input)))
+    if type(array) not in [np.ndarray, pd.core.frame.DataFrame]:
+        raise Exception('array type {} not valid'.format(type(array)))
 
-    if len(input.shape) != 2:
-        raise Exception('input must be 2D array or DataFrame')
+    if len(array.shape) != 2:
+        raise Exception('array must be 2D array or DataFrame')
 
-    if type(input) == pd.core.frame.DataFrame:
-        input = input.values
+    if type(array) == pd.core.frame.DataFrame:
+        array = array.values
 
     try:
         date_time = pd.to_datetime(date)
-    except:
-        raise Exception('unable to parse {} with pd.to_datetime()'.format(date))
+    except ValueError:
+        print('pandas.to_datetime() failed with -> {}'.format(date))
 
     # check methods and create columns list
     cols = [x for x in methods if 'percentile' not in x]
@@ -67,10 +66,10 @@ def summarize(input, date, methods, percentiles = [25,75], decimals = 3,
             if len(percentiles) != 2 or (percentiles[1] < percentiles[0]):
                 raise Exception('percentiles must [low, high]')
 
-            cols = cols + ['{}_{}'.format(method,str(percentiles[0])),
-                           '{}_{}'.format(method,str(percentiles[1]))]
+            cols = cols + ['{}_{}'.format(method, str(percentiles[0])),
+                           '{}_{}'.format(method, str(percentiles[1]))]
 
-    result = pd.DataFrame(index = [date_time], columns = cols)
+    result = pd.DataFrame(index=[date_time], columns=cols)
 
     if masks is not None:
         if type(masks) != list:
@@ -78,42 +77,47 @@ def summarize(input, date, methods, percentiles = [25,75], decimals = 3,
 
         for idx, mask in enumerate(masks):
 
-            if mask.shape != input.shape:
-                raise Exception('mask {}, {} and array {} do not '
-                                'match'.format(idx, mask.shape, input.shape))
+            if mask.shape != array.shape:
+                raise Exception('mask dimensions {} must match array '
+                                'dimensions '
+                                '{}'.format(mask.shape, array.shape))
+
+            if mask_zero_values:
+                ixz = array == 0
+                array[ixz] = np.nan
 
             mask = mask.astype('float')
             mask[mask < 1] = np.nan
-            input = input * mask
+            array = array * mask
 
     for col, method in zip(cols, methods):
 
         if 'percentile' in method:
-            c1 = '{}_{}'.format(method,str(percentiles[0]))
-            c2 = '{}_{}'.format(method,str(percentiles[1]))
-            v = getattr(np, method)(input,[percentiles[0],percentiles[1]])
+            c1 = '{}_{}'.format(method, str(percentiles[0]))
+            c2 = '{}_{}'.format(method, str(percentiles[1]))
+            v = getattr(np, method)(array, [percentiles[0], percentiles[1]])
 
             if not np.isnan(v).any():
                 v = v.round(decimals)
-                result.loc[date_time,c1] = v[0]
-                result.loc[date_time,c2] = v[1]
+                result.loc[date_time, c1] = v[0]
+                result.loc[date_time, c2] = v[1]
 
             else:
-                result.loc[date_time,c1] = np.nan
-                result.loc[date_time,c2] = np.nan
+                result.loc[date_time, c1] = np.nan
+                result.loc[date_time, c2] = np.nan
 
         else:
-            v = getattr(np, method)(input)
+            v = getattr(np, method)(array)
 
             if not np.isnan(v):
                 v = v.round(decimals)
-                result.loc[date_time,col] = v
+                result.loc[date_time, col] = v
 
     return result
 
 
 def store(values, variable, database, location, run_name, basin_id, run_id,
-          date_time, overwrite = True, units = None):
+          date_time, overwrite=True, units=None):
     '''
 
     Args
@@ -142,7 +146,7 @@ def store(values, variable, database, location, run_name, basin_id, run_id,
     if type(values) != pd.core.frame.DataFrame:
         raise Exception('values must be pandas.DataFrame')
 
-    if database not in ['sql','sqlite']:
+    if database not in ['sql', 'sqlite']:
         raise Exception('database must be "sql" or "sqlite"')
 
     if type(run_name) != str:
@@ -223,7 +227,8 @@ def store(values, variable, database, location, run_name, basin_id, run_id,
 
             insert(location, 'Inputs', fields)
 
-def get_existing_records(location, database, query_dict = None):
+
+def get_existing_records(location, database, query_dict=None):
     '''
     Get existing database records.
 
@@ -243,11 +248,11 @@ def get_existing_records(location, database, query_dict = None):
 
     '''
 
-    if database not in ['sql','sqlite']:
+    if database not in ['sql', 'sqlite']:
         raise Exception('database must be "sql" or "sqlite"')
 
     if query_dict is None:
-        query_dict = {'Inputs':['run_id','run_name','basin_id']}
+        query_dict = {'Inputs': ['run_id', 'run_name', 'basin_id']}
 
     location = make_cnx_string(location, database)
 
